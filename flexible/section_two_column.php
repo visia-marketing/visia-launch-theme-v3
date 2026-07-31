b827ece66dc1526d02a60fa09afbae60e47c7b27
Randal Pope <randalpope@Randals-MacBook-Air.local>
2026-07-31 10:24:59 -0400

    added ratio and align settings to two column

--- flexible/section_two_column.php
+++ flexible/section_two_column.php
@@ -1,48 +1,56 @@
 
 
-  <?php 
-  $column_ratio = get_sub_field('column_ratio');
-  $content_alignment = get_sub_field('content_alignment');
-
+  <?php
+  $columns_ratio = get_sub_field('columns_ratio');
+  $vertical_align_col_1 = get_sub_field('vertical_align_col_1');
+  $vertical_align_col_2 = get_sub_field('vertical_align_col_2');
 
   $left_col = '';
   $right_col = '';
 
 
-switch ($column_ratio) {
-  case 1:
+switch ($columns_ratio) {
+  case '3-9': // 25 / 75
     $left_col = 'uk-width-1-4@m';
     $right_col = 'uk-width-3-4@m';
     break;
-  case 2:
+  case '4-8': // 33 / 66
     $left_col = 'uk-width-1-3@m';
     $right_col = 'uk-width-2-3@m';
     break;
-  case 4:
+  case '5-7': // 40 / 60
+    $left_col = 'uk-width-2-5@m';
+    $right_col = 'uk-width-3-5@m';
+    break;
+  case '7-5': // 60 / 40
+    $left_col = 'uk-width-3-5@m';
+    $right_col = 'uk-width-2-5@m';
+    break;
+  case '8-4': // 66 / 33
     $left_col = 'uk-width-2-3@m';
     $right_col = 'uk-width-1-3@m';
     break;
-  case 5:
+  case '9-3': // 75 / 25
     $left_col = 'uk-width-3-4@m';
     $right_col = 'uk-width-1-4@m';
     break;
-  default:
+  default: // 6-6 — 50 / 50
     $left_col = 'uk-width-1-2@m';
-    $right_col = 'uk-width-1-2@m';  
+    $right_col = 'uk-width-1-2@m';
 }
 
 
   ?>
 
-  <div class="uk-grid uk-grid-large uk-child-width-expand">
+  <div class="uk-grid uk-grid-large uk-child-width-1-1@s" uk-grid>
 
-      <div class="uk-width-1-1@s <?php echo $left_col; ?> uk-flex-stretch content column">
-        <div class="content uk-width-1-1 uk-flex uk-flex-column uk-height-1-1 uk-flex-stretch uk-flex-<?php echo $content_alignment; ?>">
+      <div class="<?php echo $left_col; ?> uk-flex-stretch content column">
+        <div class="content uk-width-1-1 uk-flex uk-flex-column uk-height-1-1 uk-flex-stretch uk-flex-<?php echo $vertical_align_col_1; ?>">
           <?php echo get_sub_field('column_1'); ?>
         </div>
       </div>
-      <div class="k-width-1-1@s <?php echo $right_col; ?> uk-flex-stretch column">
-        <div class="content  uk-width-1-1 uk-flex uk-flex-column uk-height-1-1  uk-flex-<?php echo $content_alignment; ?>">
+      <div class="<?php echo $right_col; ?> uk-flex-stretch column">
+        <div class="content  uk-width-1-1 uk-flex uk-flex-column uk-height-1-1  uk-flex-<?php echo $vertical_align_col_2; ?>">
           <?php echo get_sub_field('column_2'); ?>
         </div>
       </div>
